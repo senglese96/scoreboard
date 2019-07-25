f12a0b2e6ce29e86e2baf9da1ea6137f9b089651
Samuel Inglese <senglese96@gmail.com>
2019-07-25 15:44:33 -0700

    begin working on front end architecture

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { AuthRoute, ProtectedRoute } from '../util/auth_route_util'
+
+const App = () => (
+    <>
+        <p>
+            It worked!
+        </p>
+    </>
+)
---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from './app'
+import { HashRouter } from 'react-router-dom'
+
+
+const Root = () => (
+    <p>It works!</p>
+)
+
+export default Root;
