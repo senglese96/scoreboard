eb377617fc35b69160086c2d302831f36e1f8dfc
Samuel Inglese <senglese96@gmail.com>
2019-08-06 16:45:33 -0700

    set up league reducer

---
+++ frontend/reducers/leagues_reducer.js
@@ -0,0 +1,25 @@
+import {
+    RECEIVE_LEAGUES,
+    RECEIVE_LEAGUE,
+    REMOVE_LEAGUE,
+} from '../actions/league_actions';
+import merge from 'lodash/merge'
+
+const leagueReducer = (state = {}, action) => {
+    Object.freeze(state)
+    switch(action.type){
+        case RECEIVE_LEAGUES:
+            return action.data
+        case RECEIVE_LEAGUE:
+            let newState = merge({}, state, { [action.data.league.id]: action.data.league })
+            return newState
+        case REMOVE_LEAGUE:
+            let someState = merge({}, state)
+            delete someState[action.leagueId]
+            return someState
+        default:
+            return state
+    }
+}
+
+export default leagueReducer;
